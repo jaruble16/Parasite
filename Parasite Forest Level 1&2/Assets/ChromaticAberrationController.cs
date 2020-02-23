48a30ee85c4c2d2bad9c47c07171d38d9952e14d
Zach Ridings <zridings@gmail.com>
2020-02-23 12:50:18 -0600

    Added Chromatic Aberration to First Person Mode

---
+++ Parasite Forest Level 1&2/Assets/ChromaticAberrationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class ChromaticAberrationController : MonoBehaviour
+{
+    public GrappleCam grappleCam;
+    public PostProcessProfile ppProfile;
+    private ChromaticAberration ca;
+
+    void Start()
+    {
+        ppProfile.TryGetSettings(out ca);
+
+        ca.intensity.value = 0;
+    }
+
+    void Update()
+    {
+        if (grappleCam.GrappleCamActive == true)
+            ca.intensity.value = Mathf.Lerp(ca.intensity.value, 1, .5f*Time.deltaTime);
+
+        else
+            ca.intensity.value = 0;
+    }
+}

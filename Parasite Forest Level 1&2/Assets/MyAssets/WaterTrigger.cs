7c3f36c5ff7c07612d284b1f33ae9972a15af343
Sean Mercer <60895318+NotSean725@users.noreply.github.com>
2020-02-25 15:31:59 -0600

    Added Water Effects when underwater

---
+++ Parasite Forest Level 1&2/Assets/MyAssets/WaterTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Underwater Visual Settings
+        RenderSettings.fog = false;
+        RenderSettings.fogColor = new Color(0.2f, 0.4f, 0.8f, 0.5f);
+        RenderSettings.fogDensity = 0.04f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RenderSettings.fog = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        RenderSettings.fog = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

290fa3cdf194454da349312ebfeb80d1b2668cb5
Jdmcc890 <55810535+Jdmcc890@users.noreply.github.com>
2020-03-05 01:55:25 -0600

    Bitch
    We Vibin'

---
+++ Parasite Forest Level 1&2/Assets/JarrettScriptStuff/Flee.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flee : MonoBehaviour
+{
+    public NavPatrol controller;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            controller.hasAggro = true;
+            controller.agent.speed = controller.agent.speed;// * 2;
+            Debug.Log("Player Trigger Aggro");
+        }
+    }
+}

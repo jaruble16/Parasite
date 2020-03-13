f72b04d52079ebc9a55168404450f30d88f4bbf2
Zach Ridings <zridings@gmail.com>
2020-03-13 17:43:24 -0500

    Progress on Respawn Mechanic

---
+++ Parasite Forest Level 1&2/Assets/RespawnActivate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnActivate : MonoBehaviour
+{
+    public SpawnPosition spawnPosition;
+    public GameObject playerCharacter;
+
+    public void Update()
+    {
+        if (playerCharacter.tag == "Dead")
+            playerCharacter.transform.position = spawnPosition.eggLocation;
+    } 
+}
---
+++ Parasite Forest Level 1&2/Assets/SpawnPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPosition : MonoBehaviour
+{
+    public GameObject egg;
+    public Vector3 eggLocation;
+
+    private void Update()
+    {
+        eggLocation = egg.transform.position;
+    }
+}

4ea020b1ad5b5ab36e8c6e47be33006b7a724db9
Zach Ridings <zridings@gmail.com>
2020-03-30 13:38:31 -0500

    Day/Night First Pass

---
+++ Parasite Forest Level 1&2/Assets/Skybox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skybox : MonoBehaviour
+{
+    public Color dayColor;
+    public Color nightColor;
+    public float duration;
+
+    private void Update()
+    {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        RenderSettings.skybox.SetColor("_Tint", Color.Lerp(dayColor, nightColor, lerp));
+    }
+}

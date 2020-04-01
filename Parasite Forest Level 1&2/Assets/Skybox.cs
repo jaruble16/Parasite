377bd80fec23b1f0d29ebfe2cef12fe6138e4f24
Zach Ridings <zridings@gmail.com>
2020-04-01 17:17:33 -0500

    Sun positioning

--- Parasite Forest Level 1&2/Assets/Skybox.cs
+++ Parasite Forest Level 1&2/Assets/Skybox.cs
@@ -7,10 +7,17 @@ public class Skybox : MonoBehaviour
     public Color dayColor;
     public Color nightColor;
     public float duration;
+    public Light sun;
+
+    private void Start()
+    {
+       
+    }
 
     private void Update()
     {
         float lerp = Mathf.PingPong(Time.time, duration) / duration;
         RenderSettings.skybox.SetColor("_Tint", Color.Lerp(dayColor, nightColor, lerp));
+        sun.transform.Rotate(Time.deltaTime * ((360/duration) / 2), 0, 0);
     }
 }

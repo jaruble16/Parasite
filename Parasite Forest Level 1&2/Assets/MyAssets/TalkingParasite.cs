4d644229f8e13466f450f25f29cf7c046991d28c
jaruble16 <55810537+jaruble16@users.noreply.github.com>
2020-04-22 17:17:25 -0500

    Added First Stone Tablet
    Created the Text UIs for all six animals on this level. Added the Stone Tablet model to the scene and created a prefab from it. Attached Daniel's talking script and added colliders to the prefab. First Stone Tablet is in the scene and functioning properly.

---
+++ Parasite Forest Level 1&2/Assets/MyAssets/TalkingParasite.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkingParasite : MonoBehaviour
+{
+    public GameObject ThoughtBubble; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+ void OnTriggerEnter(Collider collider)
+    {
+       //Check if player ran into me
+
+        //set my text bubble active
+        Debug.Log("hrrllooo");
+        ThoughtBubble.SetActive(true);
+        //Destroy()
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ThoughtBubble.SetActive(false);
+
+        //TODO make bigger colider for text interaction.
+    }
+}

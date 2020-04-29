f3ad9fa60d34e82d38cc6249e0317dc3a2aef1d1
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-04-29 15:07:00 -0500

    Added Controls Menu

---
+++ Parasite Forest Level 1&2/Assets/ControlsMenu/Code/ControlsMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControlsMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _container;
+    [SerializeField] private Button _containerButton;
+    private bool _isOpen;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        _isOpen = false;
+    }
+
+    void OnEnable()
+    {
+        _containerButton.onClick.AddListener(Close);
+    }
+
+    void OnDisable()
+    {
+        _containerButton.onClick.RemoveListener(Close);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            if (_isOpen)
+            {
+                Close();
+            }
+            else
+            {
+                Open();
+            }
+        }
+    }
+
+    void Open()
+    {
+        _container.SetActive(true);
+        _isOpen = true;
+    }
+
+    void Close()
+    {
+        _container.SetActive(false);
+        _isOpen = false;
+    }
+}
--- Parasite Forest Level 1&2/Assets/IntroCinematic/Scripts/IntroController.cs
+++ Parasite Forest Level 1&2/Assets/IntroCinematic/Scripts/IntroController.cs
@@ -35,6 +35,6 @@ public class IntroController : MonoBehaviour
 
     public void SkipScene()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
 }
